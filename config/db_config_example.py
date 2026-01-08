7f4f230df023ebd8faa9afdddd0dc6349e1528d6
Vishal Sharma <vishal.en.work@gmail.com>
2026-01-08 21:31:35 +0530

    MongoDB Added

---
+++ config/db_config_example.py
@@ -0,0 +1,27 @@
+MYSQL_CONFIG = {
+    "host": "localhost",
+    "user": "username",
+    "password": "password",
+    "database": "database_name"
+}
+
+SQL_SERVER_CONFIG = {
+    "driver": "ODBC Driver 17 for SQL Server",
+    "server": "localhost",
+    "database": "database_name",
+    "username": "username",
+    "password": "password"
+}
+
+MONGO_CONFIG = {
+    "uri": "mongodb://localhost:27017",
+    "database": "database_name"
+}
+
+AZURE_SQL_CONFIG = {
+    "driver": "ODBC Driver 17 for SQL Server",
+    "server": "your-server.database.windows.net",
+    "database": "database_name",
+    "username": "username",
+    "password": "password"
+}
